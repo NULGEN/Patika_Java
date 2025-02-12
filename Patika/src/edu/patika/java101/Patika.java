54b64a4edab0b5c4ebdab3b3a197b7760de0a2e2
Nilufer Ulgen <nilufer.korkmaz@gmail.com>
2025-02-12 10:52:52 +0300

    Patika Java 101
    Sayıları sıralama

--- Patika/src/edu/patika/java101/Patika.java
+++ Patika/src/edu/patika/java101/Patika.java
@@ -15,8 +15,49 @@ public class Patika {
        // daire();
         //manavKasa();
        // hesapMakinasi();
-        sifreGiris();
+        //sifreGiris();
+        numberSort();
 
+    }
+
+
+    public  static  void numberSort(){
+        int a,b,c;
+        Scanner input = new Scanner(in);
+        System.out.println("ilk sayıyı giriniz: ");
+        a = input.nextInt();
+        System.out.println("ikinci sayıyı giriniz: ");
+        b = input.nextInt();
+        System.out.println("üçüncü sayıyı giriniz: ");
+        c = input.nextInt();
+
+        if((a > b) && (a> c)){
+            if(b>c){
+                System.out.println("a> b> c");
+            }
+            else {
+                System.out.println("a> c> b");
+            }
+        } else if ((b >a) && (b>c)) {
+            if(a>c){
+                System.out.println("b>a>c");
+            }
+            else {
+                System.out.println("b>c>a");
+            }
+        }
+        else if((c>a) && (c>b)){
+            if(a>b){
+                System.out.println("c > a > b");
+            }
+            else {
+                System.out.println("c > b > a");
+            }
+        }
+        else{
+            System.out.println("Lütfen tam sayı giriniz!...");
+        }
+        input.close();
     }
     public static void sifreGiris(){
         String username, sifre;
